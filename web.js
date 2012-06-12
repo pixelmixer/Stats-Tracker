49a250f0d374c4321be84a4c75ef07df1583f5b4
Dustin Sparks <pixelmixer@gmail.com>
2012-06-11 22:59:18 -0400

    Added mongodb accessors

--- web.js
+++ web.js
@@ -1,3 +1,153 @@
+// Module dependencies.
+var express = require('express');
+
+var app = express.createServer();
+
+// Configuration
+app.configure( function() {
+});
+
+app.listen(3000);
+
+var databaseUrl = "mydb"; // "username:password@example.com/mydb"
+var collections = ["users", "reports", "servers"]
+var db = require("mongojs").connect(databaseUrl, collections);
+
+// app.js
+db.users.save({email: "srirangan@gmail.com", password: "iLoveMongo", sex: "male"}, function(err, saved) {
+  if( err || !saved ) console.log("User not saved");
+  else console.log("User saved");
+});
+
+// app.js
+db.users.update({email: "srirangan@gmail.com"}, {$set: {password: "iReallyLoveMongo"}}, function(err, updated) {
+  if( err || !updated ) console.log("User not updated");
+  else console.log("User updated");
+});
+
+// app.js
+db.users.update({email: "srirangan@gmail.com"}, {$set: {sex: "female"}}, function(err, updated) {
+  if( err || !updated ) console.log("User not updated");
+  else console.log("User updated");
+});
+
+app.get('/', function(req, res) {
+	//console.log( req );
+	// app.js
+	db.users.find({sex: "female"}, function(err, users) {
+		if( err || !users)
+			console.log("No female users found");
+		else users.forEach( function(femaleUser) {
+			var html = '<html><head><script type="text/javascript">console.log("'+ res +'");</script></head><body>';
+			var user;
+			user = femaleUser;
+			//console.log( femaleUser );
+			html += user.sex;
+			html += req.params;
+
+			html += '</body></html>';
+
+			res.send( html );
+		} );
+	});
+
+	
+});
+
+app.get('/registerserver/:userid/:serverip', function(req, res)
+{
+	var userid = req.params.userid;
+	var serverip = req.params.serverip;
+	db.servers.save
+	({
+		userid: userid, 
+		serverip: serverip
+	},
+	function( err, saved )
+	{
+		if( err || !saved )
+		{
+			res.send( 'Error: Not Saved...' );
+		}
+		else
+		{
+			res.send( 'Saved... ' + userid + ' ' + serverip );
+		}
+	});
+});
+
+
+
+
+/*
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/my_database');
+
+var Schema =
+	mongoose.Schema,
+	ObjectId = Schema.ObjectId;
+
+var ServerSchema = new Schema(
+{
+	hostid	: ObjectId,
+	user	: String
+});
+
+
+mongoose.model('Server', ServerSchema);
+var ServerModel = mongoose.model('Server');
+
+var server = new ServerModel({ user : 'Pixelmixer' });
+	
+
+
+	ServerModel.findOne({ user: 'Pixelmixer' }, function( err, obj )
+	{
+		console.log( obj );
+	});
+
+// Routes
+app.get('/', function(req, res) {
+	var text = server;
+	
+server.save(function (err)
+	{
+		console.log( "Helloooo" );
+	});
+
+	//MyModel.find({});
+	MyModel.findOne({ user: 'Pixelmixer' }, function ( err, doc )
+	{
+		console.log( 'response' );
+	});
+
+	console.log( ServerModel );
+
+
+
+	//console.log( ServerModel.find({}) );
+
+	res.send( text );
+});
+
+*/
+
+
+// retrieve my model
+//var ServerHost = mongoose.model('ServerHost');
+
+// create a blog post
+//var post = new ServerHost();
+
+// create a comment
+//post.push({ users: 'Pixelmixer' });
+
+//post.save(function (err) {
+//  if (!err) console.log('Success!');
+//});
+
+/*
 var express = require('express');
 var app = express();
 var server = app.listen(3000);
@@ -14,6 +164,7 @@ io.sockets.on('connection', function (socket)
 	socket.on('set nickname', function (name)
 	{
     	socket.set('nickname', name, function () { socket.emit('ready'); });
+    	socket.broadcast.emit( name + ' connected');
 	});
 
 	socket.on('msg', function (msg)
@@ -21,11 +172,13 @@ io.sockets.on('connection', function (socket)
 		socket.get('nickname', function (err, name)
 		{
 			console.log(name + ": ", msg);
+			socket.emit( 'msg', { name: name, message: msg })
 		});
 	});
 });
-
-/*var express = require('express'),
+*/
+/*
+var express = require('express'),
 	app = express.createServer(),
 	io = require('socket.io').listen(app);
 
@@ -45,8 +198,9 @@ app.get('/', function (req, res) {
 });
 
 app.listen(8000);
-
-/*var express = require('express')
+*/
+/*
+var express = require('express')
 	, app = express.createServer();
 
 app.use(express.bodyParser());
@@ -62,6 +216,7 @@ app.get('/users/:id?', function(req, res, next){
 		if (id) {
 				// do something
 				console.log( id );
+				res.send('user ' + id);
 		} else {
 				next();
 		}
@@ -71,9 +226,7 @@ app.get('/users', function(req, res){
 		// do something else
 		console.log( req );
 });
-
 */
-
 /*
 var express = require('express');
 
